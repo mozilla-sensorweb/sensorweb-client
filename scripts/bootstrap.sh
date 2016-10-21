bff8e66bbd78d2b1fdc775727f53e6af7445bd28
isabelrios <isabelrios@gmail.com>
2016-10-21 13:27:48 +0200

    fix path

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,9 +13,9 @@ export PATH=$PATH:./node_modules/.bin
 
 if [[ "$BUILD_TYPE" == "android" ]];then
     echo "Bootstrapping for android"
-    source ./build-scripts/android/bootstrap.sh
+    source ./scripts/android/bootstrap_android.sh
 elif [[ "$BUILD_TYPE" == "ios" ]]; then
-    echo "Bootstrapping for ios"
+    echo "TODO: Bootstrapping for ios"
 else
     echo "Unknown build type"
 fi
