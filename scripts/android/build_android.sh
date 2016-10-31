f540a588e3700dc691e7012881a368135a9693f6
isabelrios <isabelrios@gmail.com>
2016-10-31 11:27:11 +0100

    remove add android platform
    remove gradle check
    
    build error
    
    try to add gradle check

--- scripts/android/build_android.sh
+++ scripts/android/build_android.sh
@@ -2,16 +2,9 @@
 
 set -e
 
-cordova platform add android
-
-cordova build android
-
 cd platforms/android
 
-# check our app source code
-./gradlew app:check
-
-# exclude lint task as already done above
+# exclude lint task 
 ./gradlew build -x lint --info --console plain | tee
 
 cd -
